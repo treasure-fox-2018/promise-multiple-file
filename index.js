b3c91b1c99394f29923ec1b30dc20087ea29e626
mario <panggihmario@gmail.com>
2018-06-27 21:39:46 +0700

    multiple

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise(function(resolve,reject){
+    fs.readFile(file,"utf8",function(err,data){
+      if(err){
+        reject(err)
+      }else{
+        let parseData = JSON.parse(data)
+        resolve(parseData)
+      }
+    })
+  })
+
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+  readFilePromise(parentFileName)
+  .then(function(dataParent){
+    return readFilePromise(childrenFileName)
+      .then(function(dataChildren){
+        return ([dataParent,dataChildren])
+      })
+  })
+  .then(function(allData){
+    let parent = allData[0]
+    let children = allData[1]
+    // console.log(children)
+    // console.log(parent)
+    for(let i=0;i<parent.length;i++){
+      let arrChild = []
+      for(let j=0;j<children.length;j++){
+        if(parent[i].last_name == children[j].family){
+          arrChild.push(children[j].full_name)
+        }
+      }
+      parent[i].age = arrChild
+    }
+    console.log(parent)
+  })
+  .catch(function(err){
+    console.log("error",err)
+  })
+  
 }
 
 matchParentsWithChildrens('./parents.json', './childrens.json');
