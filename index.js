5979ec5233d8e7b1d3e1a3ac5ca35a7b77873448
wisnugautama <ngurahwisnugautama@gmail.com>
2018-06-27 23:22:49 +0700

    release 0 DONE

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function readFilePromise() {
+function readFilePromise(file) {
   // psst, the promise should be around here...
+  return new Promise((resolve,reject) => {
+    fs.readFile(file,(err,data) => {
+      if (err) {
+        reject(`file tidak ada`)
+      }else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
-console.log("Notification : Data sedang diproses !");
+// matchParentsWithChildrens('./parents.json', './childrens.json');
+// console.log("Notification : Data sedang diproses !");
 
-// for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+// // for Release 2
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+// matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+
+readFilePromise('./parents.json')
+.then((read_succees) => {
+  console.log(read_succees);
+})
+.catch((read_error) => {
+  console.log(read_error)
+})
