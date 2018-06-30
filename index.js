fd73487315ce4398fe81583baeec787d31c48e0d
Your Name <fahrisyany@gmail.com>
2018-06-30 23:34:03 +0700

    done

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function readFilePromise() {
+function sleep(milliseconds) {
+
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+
+}
+
+function readFilePromise(JSONfile) {
   // psst, the promise should be around here...
+  return new Promise(function (resolve, reject) {
+
+    fs.readFile(JSONfile, function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        let arr = JSON.parse(data)
+        resolve(arr)
+        console.log(data)
+        sleep(3000)
+      }
+    })
+  })
 }
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
   // your code here... (p.s. readFilePromise function(s) should be around here..)
+
+  readFilePromise(parentFileName)
+    .then(function (parentsData) {
+      return readFilePromise(childrenFileName)
+        .then(function (childrensData) {
+
+          parentsData.forEach(p_Search => {
+            p_Search.childrensData = []
+            childrensData.forEach(c_Search => {
+              if (p_Search.last_name === c_Search.family) {
+                p_Search.childrensData.push(c_Search.full_name);
+
+              }
+            })
+            console.log(p_Search)
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    });
+
+
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
+matchParentsWithChildrens((__dirname + '/parents.json'), (__dirname + '/childrens.json'));
 console.log("Notification : Data sedang diproses !");
 
 // for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+// matchParentsWithChildrens((__dirname + '/parents.json'),(__dirname + '/not_childrens.json'));
+// matchParentsWithChildrens((__dirname + '/not_parents.json'), (__dirname + '/not_childrens.json'));
