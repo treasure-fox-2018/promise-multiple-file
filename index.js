72dfa781ed67e963b67fde27d86ff22d5c8450df
kneric <brian.pradipta.a@gmail.com>
2018-07-01 22:45:33 +0700

    first init

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 const fs = require('fs');
-var sleep = require('sleep');
+const sleep = require('sleep'); 
 
-function readFilePromise() {
-  // psst, the promise should be around here...
+function readFilePromise(file) {
+  return new Promise ((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject (err);
+      } else {
+        resolve (JSON.parse(data));
+      }
+    })
+  })
 }
+
+// readFilePromise('./parents.json')
+// .then(result => {
+//   console.log(result)
+// })
 
 function matchParentsWithChildrens(parentFileName, childrenFileName) {
-  // your code here... (p.s. readFilePromise function(s) should be around here..)
+  
+  readFilePromise(parentFileName)
+  .then(result => {
+    sleep.sleep(5);
+    var parent_data = result;
+
+    readFilePromise(childrenFileName)
+    .then(result => {
+      sleep.sleep(5);
+      var childrens = result;
+
+      for(let i in childrens){
+        for (let j in parent_data){
+          if (childrens[i].family == parent_data[j].last_name){
+            if (!parent_data[j].children){
+              parent_data[j].children = [childrens[i].full_name];
+            } else {
+              parent_data[j].children.push(childrens[i].full_name);
+            }
+          }
+        }
+      }
+      console.log(parent_data)
+    })
+    .catch (err =>{
+      console.log('Error reading data')
+    })
+  })
+  .catch (err =>{
+    console.log('Error reading data')
+  })
 }
 
-matchParentsWithChildrens('./parents.json', './childrens.json');
-console.log("Notification : Data sedang diproses !");
+// matchParentsWithChildrens('./parents.json', './childrens.json');
+// console.log("Notification : Data sedang diproses !");
 
-// for Release 2
-matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
-matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json');
+
+// matchParentsWithChildrens('./parents.json', './not_a_real_file.json');
+matchParentsWithChildrens('./not_a_real_file.json', './also_not_a_real_file.json')
